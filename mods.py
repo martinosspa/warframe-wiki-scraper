8035468bda9630b346ba147cb3a6ad7b461e7235
martinosspa <35999764+martinosspa@users.noreply.github.com>
2019-09-27 15:56:51 +0300

    a

--- mods.py
+++ mods.py
@@ -3,9 +3,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from pprint import pprint
 import csv
-
-
+import asyncio
+import aiohttp
+import grequests
+base_url = 'https://warframe.fandom.com/wiki'
 url = 'https://warframe.fandom.com/wiki/Mod'
+
+mod_exceptions = ['Astral Autopsy', 'Scorch', 'Seeker', 'Coolant Leak', 'Sanctuary', 'Antitoxin']
+
 resp = urlopen(url)
 
 soup = BeautifulSoup(resp, 'html.parser')
@@ -23,9 +28,11 @@ specific_headers = []
 for table in tables:
 	headers = table.text.split('\n\n')[1].split('\n')
 	headers.remove('')
+
 	if 'Polarity' in headers:
 		headers.remove('Polarity')
 	for h in headers:
+		h = 'Category' if h == 'Subcategory' else h
 		if not h in all_headers:
 			# special case
 			if not h == ' Description':
@@ -35,7 +42,6 @@ for table in tables:
 
 # mod handling
 mods = []
-
 
 # normal tables
 for table_pos, table in enumerate(tables):
@@ -51,8 +57,13 @@ for table_pos, table in enumerate(tables):
 	# filter out u2009
 	text = [info.replace('\u2009', '') for info in text]
 
+	# filter out all spaces at the start of info
+	text = [item.replace(' ', '', 1) if item.startswith(' ') else item for item in text]
+
 	# polarities for the table
 	polarities = [pol.img.attrs['alt'].replace(' Pol', '') for pol in table.find_all('a', title='Polarity')]
+
+
 	# special case
 	if title == 'Melee':
 		_special_case_pos = text.index('Focused Defense')
@@ -65,31 +76,71 @@ for table_pos, table in enumerate(tables):
 		mod['Polarity'] = polarities[(info_pos // header_size)]
 		for header_pos in range(header_size):
 			header = specific_headers[table_pos][header_pos]
-			# special case
-			if header == ' Description':
-				header = 'Description'
-			mod[header] = text[info_pos + header_pos]
+			entry_text = text[info_pos + header_pos]
+
+			header = 'Description' if header == ' Description' else header
+
+			# exilus
+			entry_split = entry_text.split(', ')
+			if 'Exilus' in entry_split:
+				if header == 'Category':
+					entry_split.pop(0)
+					mod['Subcategory'] = ''.join(entry_split)
+				if title == 'Warframe':
+					mod['Type'] = 'Exilus'
+			else:
+				# default haddling
+				mod[header] = entry_text
 		mods.append(mod)
 
-# augment tables
 
+
+
+# augment tables
+'''
 for table_pos, table in enumerate(special_tables):
 	text = table.text.split('\n')
 	text = list(filter(lambda info: not info == '', text))
 	text = list(filter(lambda info: not info == ' ', text))
 	text = [info.replace('\u2009', '') for info in text]
-	del text[0]
-	for item in text:
-		print(item)
+	#del text[0]
+	print(text)
 	# split based on volt 
 
+'''
 
 
 
-
+all_headers.insert(3, 'Drain')
 all_headers.insert(2, 'Polarity')
+all_headers.insert(4, 'Subcategory')
 
-#print(headers)
+# sorts mods based on type
+mods = sorted(mods, key=lambda mod: mod['Type'])
+
+# filter out pvp mods
+for mod in mods:
+	if 'Exclusive to PvP' in mod['Description']:
+		mods.remove(mod)
+
+# gets polarities
+reqs = [grequests.get(f'{base_url}/{item_name}') for item_name in [mod['Name'].replace(' ', '_') for mod in mods]]
+responses = resp = grequests.map(reqs)
+
+for resp_pos, resp in enumerate(responses):
+	if not mods[resp_pos]['Name'] in mod_exceptions and not mods[resp_pos]['Type'] == 'Stance':
+		s = BeautifulSoup(resp.text, 'html.parser')
+		t = s.find('table', 'emodtable')
+		text_ = list(filter(lambda info: not info == '', t.text.split('\n')))
+		#aura_multiplier = -1 if mods[resp_pos]['Type'] == 'Aura' else 1
+
+		if mods[resp_pos]['Type'] == 'Aura':
+			mods[resp_pos]['Drain'] = int(text_[-1][-1]) * -1
+		else:
+			mods[resp_pos]['Drain'] = text_[-1][-1]
+	else:
+		mods[resp_pos]['Drain'] = 'Error'
+# write csv
 with open('mods.csv', 'w', encoding='utf-16', newline='') as f:
 	writer = csv.DictWriter(f , all_headers)
 	writer.writeheader()
--- test.py
+++ test.py
@@ -1,30 +1,135 @@
-import json
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from pprint import pprint
-import csv
-url = 'https://warframe.fandom.com/wiki/Mod'
-resp = urlopen(url).read()
+from random import random
+from numpy.random import choice
+from math import ceil
 
-soup = BeautifulSoup(resp, 'html.parser')
-mods = []
-
-tables = soup.find_all('div', class_='tabbertab')
-for _ in range(6):
-	tables.pop(len(tables) - 1)
-
-for table in tables:
-	info = table.find_all('a', title='Polarity')
-	for pol in info:
-		print(pol.img.attrs['alt'].replace(' Pol', ''))
+_base_stats = ['impact', 'puncture' ,'slash']
+class Enemy:
+	def __init__(self, level):
+		h = 300 * (1 + (pow(level - 8, 2)) * 0.015)
+		self.armor = 500 * (1 + pow(level - 8, 1.75) * 0.005)
+		self.damage_reduction = 1 - (self.armor / (self.armor + 300))
+		self.health = h * self.damage_reduction
+	def __repr__(self):
+		return str(self.health)
 
 
+class Weapon:
+	def __init__(self,
+				impact=0,
+				puncture=0,
+				slash=0,
+				cold=0,
+				electricity=0,
+				heat=0,
+				toxin=0,
+				blast=0,
+				corrosive=0,
+				gas=0,
+				magnetic=0,
+				radiation=0,
+				viral=0,
+				crit_chance=0,
+				crit_multiplier=0,
+				status_chance=0,
+				fire_rate=0,
+				clip_ammo=0,
+				multishot=0,
+				reload_time=0):
 
-#print(headers)
-'''
-with open('mods.csv', 'w', encoding='utf-16', newline='') as f:
-	writer = csv.DictWriter(f , headers)
-	writer.writeheader()
-	for mod in mods:
-		writer.writerow(mod)
-'''
+		self.damage_types = {
+		'impact' : impact,
+		'puncture' : puncture,
+		'slash' : slash,
+		'cold' : cold,
+		'electricity' : electricity,
+		'heat' : heat,
+		'toxin' : toxin,
+		'blast' : blast,
+		'corrosive' : corrosive,
+		'gas' : gas,
+		'magnetic' : magnetic,
+		'radiation' : radiation,
+		'viral' : viral
+		}
+
+		self.damage_distributions = {
+		'impact' : impact * 4,
+		'puncture' : puncture * 4,
+		'slash' : slash * 4,
+		'cold' : cold,
+		'electricity' : electricity,
+		'heat' : heat,
+		'toxin' : toxin,
+		'blast' : blast,
+		'corrosive' : corrosive,
+		'gas' : gas,
+		'magnetic' : magnetic,
+		'radiation' : radiation,
+		'viral' : viral
+		}
+
+
+		# base elements * 4 + others status
+		self.total_proportional_damage = sum([self.damage_distributions[p] for p in self.damage_distributions])
+		self.proportial_distributions = {p : self.damage_distributions[p] / self.total_proportional_damage for p in self.damage_distributions}
+		self.total_damage = sum([self.damage_types[p] for p in self.damage_types])
+		
+		self.crit_chance = crit_chance
+		self.crit_multiplier = crit_multiplier
+		self.status_chance = status_chance/100
+		self.fire_rate = fire_rate
+		self.clip_ammo = clip_ammo
+		self.multishot = multishot
+		self.reload_time = reload_time
+
+	def simulate_hit(self, enemy):
+		status_roll = random()
+
+
+
+		if self.status_chance >= status_roll:
+			# status proc
+			proc = choice([p for p in self.damage_distributions], 
+							1, 
+							p = [self.proportial_distributions[p] for p in self.proportial_distributions])
+			proc = proc[0]
+
+			#print(proc)
+			
+			if proc == 'slash':
+				pass
+		enemy.health = enemy.health - self.total_damage
+
+		sum_ = sum(self.damage_types[k] for k in _base_stats)
+		q = sum_ / 16
+
+		for base_element in _base_stats:
+			
+			d = ceil(self.damage_types[base_element] / q)*q
+			#print(self.damage_types[base_element])
+			#print(q)
+			print(d)
+		#damage_to_enemy = 300/()
+		#DM=300300+AR(1−AM)(1+AM)(1+HM)
+
+
+
+
+
+
+
+
+weapon = Weapon(status_chance=10, impact=30, puncture=30, slash=40)
+
+#print(weapon.total_damage)
+enemy = Enemy(100)
+#print(weapon.total_damage * enemy.damage_reduction)
+weapon.simulate_hit(enemy)
+#print(enemy)
+
+
+
+
+
+
+#https://warframe.fandom.com/wiki/Status_Effect
